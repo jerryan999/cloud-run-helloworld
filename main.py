86d290e32f1187799b136476cb72789c3e45583a
zhibin.an <zhibin.an@castbox.fm>
2022-01-20 20:06:11 +0800

    init commit

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
+import base64
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = request.args.get("name", "world")
     return "Hello {}!".format(name)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
